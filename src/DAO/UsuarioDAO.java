fd30becb03681f55ccdc6b5ef88131f20a37b433
YingLin <linying13141314@gmail.com>
2020-11-01 16:51:16 +0100

    Proyecto modificado con control de errores

--- src/DAO/UsuarioDAO.java
+++ src/DAO/UsuarioDAO.java
@@ -1,9 +1,6 @@
 package DAO;
 
 import Models.AbstractUsuario;
-import Models.Venta;
-
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,7 +20,9 @@ public class UsuarioDAO extends AbstractDAO {
         super.conectar();
 
         AbstractUsuario miUsuario = null;
-        String sql = "SELECT * from usuario where contrasena = ? and nombreUsuario=?";
+
+        //binary sirve para diferenciar entre mayuscula y minuscula. Prohibe loguearse aunque tiene el mismo caracter.
+        String sql = "SELECT * from usuario where binary contrasena = ? and binary nombreUsuario=?";
 
         PreparedStatement ps = conexion.prepareStatement(sql);
         ps.setString(1,pass);
