a748e6e0e21d7a483d8d4f0546e28ce4223814de
clcriado0 <carlos.larriba@metadata.es>
2020-11-02 19:25:43 +0100

    Creación del VentaAltaCliente.fxml, ControladorVentaAltaCliente.java, VehiculoDAO.java y modificación del Main.java, ControladorLogin.java y ContrladorVenta.java.

---
+++ src/Controllers/ControladorVentaAltaCliente.java
@@ -0,0 +1,39 @@
+package Controllers;
+
+import Indice.Main;
+import Models.AbstractUsuario;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class ControladorVentaAltaCliente {
+
+    //VentaAltaCliente.FXML
+    @FXML
+    Button buttonCancelar;
+    @FXML
+    Button buttonAceptar;
+
+    private Main miApp;
+
+    AbstractUsuario miUsuario;
+
+    public ControladorVentaAltaCliente() {
+    }
+
+    public void changeScene(ActionEvent e) throws IOException {
+        String ruta = "";
+        if(e.getSource().equals(buttonCancelar)) {
+            ruta = "/View/Venta.fxml";
+        } else if (e.getSource().equals(buttonAceptar)){
+            ruta = "/View/Venta.fxml";
+        }
+        FXMLLoader pane = new FXMLLoader(getClass().getResource(ruta));
+        miApp.getPrimaryStage().setScene(new Scene(pane.load(), 1280, 720));
+    }
+
+}
